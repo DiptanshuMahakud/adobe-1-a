906f0194d3d1869b020474be627b7ec108eeaef7
Rishabh Sinha <02sinharishabh15@gmail.com>
2025-07-28 23:55:19 +0530

    Model Path Fixed

--- model/classifier.py
+++ model/classifier.py
@@ -1,15 +1,19 @@
+from pathlib import Path
 from transformers import AutoTokenizer, AutoModelForSequenceClassification, TextClassificationPipeline
 
-model_dir = "./bin"  
+model_dir = Path(__file__).resolve().parent / "bin"
 
-tokenizer = AutoTokenizer.from_pretrained(model_dir, local_files_only=True)
-model = AutoModelForSequenceClassification.from_pretrained(model_dir, local_files_only=True)
+tokenizer = AutoTokenizer.from_pretrained(model_dir, local_files_only=False)
+model = AutoModelForSequenceClassification.from_pretrained(model_dir, local_files_only=False)
 
 classifier = TextClassificationPipeline(
     model=model,
     tokenizer=tokenizer,
     top_k=None,
-    device=-1
+    device=-1,
+    truncation=True,  
+    max_length=512,   
+    padding=True 
 )
 
 def classify(inp:str):
